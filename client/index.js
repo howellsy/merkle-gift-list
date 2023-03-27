945fb6fa1986d96f8baf46cb1edfe00b78a2b4c8
howellsy <jamie.howells@gmail.com>
2023-03-27 17:01:31 +0100

    Client: POST name to server with Merkle proof

--- client/index.js
+++ client/index.js
@@ -5,10 +5,15 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list?
+  const name = process.argv[2] || 'Chris Windler';
+
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
   console.log({ gift });
